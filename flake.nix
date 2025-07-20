8a4ccc0e832fa3abd7993104de1096987d04672c
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-07-21 00:16:56 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,3 @@
-# To make the image, do:
-# $ sudo ${full path to}/nix run ".?submodules=1#nixosConfigurations.nixos-wsl-desktop.config.system.build.tarballBuilder" --impure
-# On first boot: do as nix.trusted-users
-# $ sudo nix-channel --update
-
-# And build nixos + home-manager:
-# $ sudo -E nixos-rebuild switch --flake ".?submodules=1#nixos-wsl-desktop" --impure
-# (impure is required!)
-
-# profiles/work is encrypted via git-crypt.
-# use "git-crypt unlock <key-file>" at project root.
-#
-# Note: if want to lock, "git init" and "git lock -k <key-file>"
-# https://github.com/AGWA/git-crypt/issues/62#issuecomment-1399340304
-
 {
   inputs = {
     nixos.url = "github:nixos/nixpkgs/nixos-25.05";
