2f7f43e5890341e9346b70ac7c2151fc65acf2d1
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-11-02 22:30:21 +0900

    feat(agent): add fail2ban service configuration for enhanced security

--- modules/management/agent.nix
+++ modules/management/agent.nix
@@ -11,8 +11,22 @@ in
         AllowUsers = [ cfg.sshUser ];
         PermitRootLogin = "no";
         PasswordAuthentication = false;
-        # TODO: Change port
       };
     };
+    services.fail2ban = {
+      enable = true;
+      maxretry = 5;
+      ignoreIP = [
+        "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"
+        "8.8.8.8"
+      ];
+      bantime = "24h";
+      bantime-increment = {
+        enable = true;
+        multipliers = "1 2 4 8 16 32 64";
+        maxtime = "168h";
+      };
+    };
+    boot.kernelModules = ["xt_multiport" ]; # https://www.reddit.com/r/NixOS/comments/1e2micc/comment/mcbbv1y/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
   };
 }
