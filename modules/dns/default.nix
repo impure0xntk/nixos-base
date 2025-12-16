889ac7647ce2c47352fc27cdec6961acb3fe9c90
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-12-16 22:11:51 +0900

    refactor(dns): remove container setting

--- modules/dns/default.nix
+++ modules/dns/default.nix
@@ -59,122 +59,102 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    boot.enableContainers = true;
-    containers.dns-server = {
-      autoStart = true;
+    # Must use systemd-creds to load TLS credentials because of systemd sandboxing
+    # Similar: https://mynixos.com/nixpkgs/option/services.godns.settings
+    systemd.services.blocky.serviceConfig = {
+      LoadCredential = (lib.optionals (cfg.certFile != null) [
+        "cert.pem:${cfg.certFile}"
+      ]) ++ (lib.optionals (cfg.keyFile != null) [
+        "key.pem:${cfg.keyFile}"
+      ]);
+    };
 
-      bindMounts = {
-        "${certDir}/cert.pem" = lib.mkIf (cfg.certFile != null) {
-          hostPath = cfg.certFile;
-          isReadOnly = true;
+    services.blocky = {
+      enable = cfg.enable;
+      settings = {
+        certFile = lib.optionalString (cfg.certFile != null) "/run/credentials/blocky.service/cert.pem";
+        keyFile = lib.optionalString (cfg.keyFile != null) "/run/credentials/blocky.service/key.pem";
+        log = {
+          level = "debug";
+          privacy = true;
         };
-        "${certDir}/key.pem" = lib.mkIf (cfg.keyFile != null) {
-          hostPath = cfg.keyFile;
-          isReadOnly = true;
+        ports = {
+          dns = 53;
+          # http = 4000;
+          # https = 8443; # to avoid conflict with reverse-proxy
+          tls = 853;
         };
-      };
-
-
-      config = {config, pkgs, lib, ...}: {
-        imports = [ ../core/minimal.nix ];
-        system.stateVersion = config.system.nixos.release;
-
-        services.journald.extraConfig = ''
-          SystemMaxUse=100M
-        '';
-
-        # Must use systemd-creds to load TLS credentials because of systemd sandboxing
-        # Similar: https://mynixos.com/nixpkgs/option/services.godns.settings
-        systemd.services.blocky.serviceConfig = {
-          LoadCredential = [
-            "cert.pem:${certDir}/cert.pem"
-            "key.pem:${certDir}/key.pem"
-          ];
-        };
-
-        services.blocky = {
-          enable = cfg.enable;
-          settings = {
-            certFile = lib.optionalString (cfg.certFile != null) "/run/credentials/blocky.service/cert.pem";
-            keyFile = lib.optionalString (cfg.keyFile != null) "/run/credentials/blocky.service/key.pem";
-            log = {
-              level = "debug";
-              privacy = true;
-            };
-            ports = {
-              dns = 53;
-              # http = 4000;
-              # https = 8443; # to avoid conflict with reverse-proxy
-              tls = 853;
-            };
-            upstreams = {
-              strategy = "strict";
-              init.strategy = "fast";
-              groups = {
-                default = [
-                  "https://dns11.quad9.net/dns-query" # Quad9 DoH Secured w/ECS: Malware blocking, DNSSEC Validation, ECS enabled
-                ];
-              };
-            };
-            customDNS.mapping = cfg.customDomains;
-            blocking = {
-              loading.strategy = "fast";
-              denylists = {
-                general = [
-                  "https://sebsauvage.net/hosts/hosts"
-                  "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts" # Unified + fakenews + gambling
-                  "https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild2_big.txt"
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/hosts/ultimate.txt"
-                ];
-                ads = [
-                  "https://blocklistproject.github.io/Lists/ads.txt"
-                  # Youtube
-                  "https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/youtubelist.txt"
-                  # Regional Ads (Japan)
-                  "https://warui.intaa.net/adhosts/hosts.txt"
-                  "https://raw.githubusercontent.com/lawnn/adaway-hosts/refs/heads/master/hosts.txt"
-                  "https://raw.githubusercontent.com/PepperCat-YamanekoVillage/LINE-Ad-Block/refs/heads/main/list.txt" # LINE Ads
-                ];
-                tracking = [
-                  "https://blocklistproject.github.io/Lists/tracking.txt"
-                  "https://github.com/KnightmareVIIVIIXC/AIO-Firebog-Blocklists/raw/main/hostslists/firebogtrack.txt"
-                ];
-                malicious = [
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/hosts/tif.txt"
-                  "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
-                  "https://github.com/KnightmareVIIVIIXC/AIO-Firebog-Blocklists/raw/main/hostslists/firebogmal.txt"
-                ];
-                annoyance = [
-                  "https://github.com/KnightmareVIIVIIXC/AIO-Firebog-Blocklists/raw/main/hostslists/firebogsus.txt"
-                  "https://raw.githubusercontent.com/bigdargon/hostsVN/refs/heads/master/hosts"
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling.txt"
-                ];
-                misc = [
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/doh-vpn-proxy-bypass.txt"
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/dyndns.txt"
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/spam-tlds-onlydomains.txt"
-                ];
-                strict = [
-                  "https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild2_nsfw.txt"
-                  "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/nsfw.txt"
-                  "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-social-only/hosts" # porn + social
-                ];
-              };
-              clientGroupsBlock = {
-                default = [
-                  "general"
-                  "ads"
-                  "tracking"
-                  "malicious"
-                  "annoyance"
-                  "misc"
-                ]; } // (lib.optionalAttrs (lib.length cfg.blocklists > 0) (
-                lib.listToAttrs (map (item: lib.nameValuePair item.host item.category) cfg.blocklists)
-              ));
-            };
-            queryLog.type = "console";
+        upstreams = {
+          strategy = "strict";
+          init.strategy = "fast";
+          groups = {
+            default = [
+              "https://dns11.quad9.net/dns-query" # Quad9 DoH Secured w/ECS: Malware blocking, DNSSEC Validation, ECS enabled
+            ];
           };
         };
+        customDNS.mapping = cfg.customDomains;
+        blocking = {
+          loading.strategy = "fast";
+          denylists = {
+            general = [
+              "https://sebsauvage.net/hosts/hosts"
+              "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts" # Unified + fakenews + gambling
+              "https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild2_big.txt"
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/hosts/ultimate.txt"
+            ];
+            ads = [
+              "https://blocklistproject.github.io/Lists/ads.txt"
+              # Youtube
+              "https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/youtubelist.txt"
+              # Regional Ads (Japan)
+              "https://warui.intaa.net/adhosts/hosts.txt"
+              "https://raw.githubusercontent.com/lawnn/adaway-hosts/refs/heads/master/hosts.txt"
+              "https://raw.githubusercontent.com/PepperCat-YamanekoVillage/LINE-Ad-Block/refs/heads/main/list.txt" # LINE Ads
+            ];
+            tracking = [
+              "https://blocklistproject.github.io/Lists/tracking.txt"
+              "https://github.com/KnightmareVIIVIIXC/AIO-Firebog-Blocklists/raw/main/hostslists/firebogtrack.txt"
+            ];
+            malicious = [
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/hosts/tif.txt"
+              "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+              "https://github.com/KnightmareVIIVIIXC/AIO-Firebog-Blocklists/raw/main/hostslists/firebogmal.txt"
+            ];
+            annoyance = [
+              "https://github.com/KnightmareVIIVIIXC/AIO-Firebog-Blocklists/raw/main/hostslists/firebogsus.txt"
+              "https://raw.githubusercontent.com/bigdargon/hostsVN/refs/heads/master/hosts"
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling.txt"
+            ];
+            misc = [
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/doh-vpn-proxy-bypass.txt"
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/dyndns.txt"
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/spam-tlds-onlydomains.txt"
+            ];
+            strict = [
+              "https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild2_nsfw.txt"
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/nsfw.txt"
+              "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-social-only/hosts" # porn + social
+            ];
+          };
+          allowlists = {
+            general = [
+              "fc.yahoo.com" # For yfinance
+            ];
+          };
+          clientGroupsBlock = {
+            default = [
+              "general"
+              "ads"
+              "tracking"
+              "malicious"
+              "annoyance"
+              "misc"
+            ]; } // (lib.optionalAttrs (lib.length cfg.blocklists > 0) (
+            lib.listToAttrs (map (item: lib.nameValuePair item.host item.category) cfg.blocklists)
+          ));
+        };
+        queryLog.type = "console";
       };
     };
   };
