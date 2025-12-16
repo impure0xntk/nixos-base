f99b5faf78353a0dcecba4e5684b9331e21dc029
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-12-16 22:16:37 +0900

    feat(ai): nemotoron 3 nano

--- modules/ai/litellm/models/others.nix
+++ modules/ai/litellm/models/others.nix
@@ -21,6 +21,12 @@
       params = {};
     }
   ];
+  "nemotoron-3-nano" = [
+    {
+      model = "openrouter/nvidia/nemotron-3-nano-30b-a3b:free";
+      params = {};
+    }
+  ];
   text-to-speech-playai-tts = [
     {
       model = "groq/playai-tts";
