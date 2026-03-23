0c345304cf42c7f8a64d737e8d7ca941a17733c6
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-03-23 17:51:02 +0900

    feat: chat: matrix-tuwunel

---
+++ modules/chat/default.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.my.system.chat;
+
+  # https://matrix-construct.github.io/tuwunel/deploying/nixos.html#jemalloc-and-hardened-profile
+/*   package = pkgs.pure-unstable.matrix-tuwunel.override {
+    enableJemalloc = false;
+  }; */
+  package = pkgs.pure-unstable.matrix-tuwunel;
+in
+{
+  options.my.system.chat = {
+    enable = lib.mkEnableOption "Enable Matrix Conduit server";
+    serverName = lib.mkOption {
+      type = lib.types.str;
+      description = "The server name for Matrix Conduit.";
+      default = "";
+    };
+    host = lib.mkOption {
+      type = lib.types.str;
+      description = "The host for Matrix Conduit.";
+      default = "127.0.0.1";
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      description = "The port for Matrix Conduit.";
+      default = 6167;
+    };
+    extraGlobalSettings = lib.mkOption {
+      type = lib.types.attrs;
+      description = "Extra settings for Matrix Conduit.";
+      default = {};
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.matrix-tuwunel = {
+      enable = true;
+      package = package;
+      settings.global = {
+        address = [ cfg.host ];
+        port = [ cfg.port ];
+        server_name = cfg.serverName;
+      } // cfg.extraGlobalSettings;
+    };
+  };
+}
