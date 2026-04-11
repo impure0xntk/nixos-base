037b442ddb3568bc912e2b9f4abf1b9c03f9dfa5
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-04-11 16:37:26 +0900

    feat(ai/models): add role play model

--- modules/ai/litellm/models/qwen.nix
+++ modules/ai/litellm/models/qwen.nix
@@ -16,6 +16,15 @@ let
       { model = "openai/qwen/qwen3.5-397b-a17b"; params = nanogptParams; }
     ];
     # For role play
+    "qwen3.5-bluestar" = [
+      { model = "openai/Qwen3.5-27B-BlueStar-Derestricted"; params = nanogptParams; }
+    ];
+    "qwen3.5-bluestar-light" = [
+      { model = "openai/Qwen3.5-27B-BlueStar-Derestricted-Lite"; params = nanogptParams; }
+    ];
+    "qwen3.5-bluestar-v2" = [
+      { model = "openai/Qwen3.5-27B-BlueStar-v2-Derestricted"; params = nanogptParams; }
+    ];
     "qwen3.5-bluestar-v2-light" = [
       { model = "openai/Qwen3.5-27B-BlueStar-v2-Derestricted-Lite"; params = nanogptParams; }
     ];
