62127bf1ae8e5c750fa67869ceb5c93d250be1bb
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-07-06 23:01:10 +0900

    feat(ai/models): update openrouter models

--- modules/ai/litellm/models/others.nix
+++ modules/ai/litellm/models/others.nix
@@ -1,19 +1,22 @@
 { lib, opencodeZenParams, nanogptParams, ... }:
 
 {
-  "owl-alpha" = [
-    { model = "openrouter/openrouter/owl-alpha"; }
-  ];
   "laguna-m.1" = [
     { model = "openrouter/poolside/laguna-m.1:free"; }
   ];
-  "nemotoron-3-super" = [
+  "laguna-xs-2.1" = [
+    { model = "openrouter/poolside/laguna-xs-2.1:free"; }
+  ];
+  "north-mini-code" = [
+    { model = "openrouter/cohere/north-mini-code:free"; }
+  ];
+  "nemotron-3-super" = [
     { model = "openrouter/nvidia/nemotron-3-super-120b-a12b:free"; }
   ];
-  "nemotoron-3-ultra" = [
+  "nemotron-3-ultra" = [
     { model = "openrouter/nvidia/nemotron-3-ultra-550b-a55b:free"; }
   ];
-  "nemotoron-embed" = [
+  "nemotron-embed" = [
     { model = "openrouter/nvidia/llama-nemotron-embed-vl-1b-v2:free"; }
   ];
   "minimax-m2.5" = [
